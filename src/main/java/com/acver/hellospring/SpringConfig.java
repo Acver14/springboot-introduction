6e263baedaf5b734341313037136017c84629d67
Acver <devacver@gmail.com>
2022-08-15 21:34:13 +0900

    2022.08.15 8th lesson

--- src/main/java/com/acver/hellospring/SpringConfig.java
+++ src/main/java/com/acver/hellospring/SpringConfig.java
@@ -1,24 +1,37 @@
 package com.acver.hellospring;
 
 import com.acver.hellospring.domain.Member;
+import com.acver.hellospring.repository.JdbcTemplateMemberRepository;
 import com.acver.hellospring.repository.MemberRepository;
 import com.acver.hellospring.repository.MemoryMemberRepository;
 import com.acver.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 import java.util.List;
 import java.util.Optional;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
 
+
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
